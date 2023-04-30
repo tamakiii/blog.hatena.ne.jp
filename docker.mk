ecf423eaffe0d14b72eed7656bae12453124f775
tamakiii <tamakiii@users.noreply.github.com>
2023-04-30 15:58:01 +0900

    mount volume

--- docker.mk
+++ docker.mk
@@ -2,9 +2,10 @@
 
 include function.mk
 
+export USERNAME ?= $(call env,USERNAME)
+export REPOSITORY ?= $(call env,REPOSITORY)
 export IMAGE ?= $(call env,IMAGE)
 export TAG := $(call env,TAG)
-PLATFORM := $(uname )
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -16,7 +17,7 @@ build: Dockerfile
 	docker build -t $(IMAGE):$(TAG) .
 
 bash: build
-	docker run --rm -T $(IMAGE):$(TAG) $@
+	docker run --rm -it -v $(PWD):/opt/$(USER)/$(REPOSITORY) -w /opt/$(USER)/$(REPOSITORY) $(IMAGE):$(TAG) $@
 
 teardown:
 	$(MAKE) -f env.mk $@
